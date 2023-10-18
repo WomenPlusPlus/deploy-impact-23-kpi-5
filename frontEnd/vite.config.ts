ba218fbc96c7640c1d8944cc01e6ead0925d6dbf
dealmeida-dev <102060897+dealmeida-dev@users.noreply.github.com>
2023-10-18 17:35:00 +0200

    fix deployment errors

--- frontEnd/vite.config.ts
+++ frontEnd/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/deploy-impact-23-kpi-5/", // Set your repository name here
+  base: "/deploy-impact-23-kpi-5/",
 });
