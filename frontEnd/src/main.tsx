95b83e4cc1a4b4f1ef66c89d88053b166bc08d72
dealmeida-dev <102060897+dealmeida-dev@users.noreply.github.com>
2023-10-23 13:43:35 +0200

    fix: app not displaying

--- frontEnd/src/main.tsx
+++ frontEnd/src/main.tsx
@@ -1,37 +1,4 @@
-// import React from "react";
-// import {
-//   Route,
-//   BrowserRouter as Router,
-//   Routes,
-//   useNavigate,
-// } from "react-router-dom";
-// import EconomistLandingPage from "./src/components/EconomistLandingPage";
-// import GatekeeperLandingPage from "./src/components/GatekeeperLandingPage";
-// import Form from "./src/components/Form";
-// import AuthenticationPage from "./src/components/AuthenticationPage";
-// import NotFoundPage from "./src/components/NotFoundPage"; // Make sure to import NotFoundPage
+import ReactDOM from "react-dom";
+import App from "./App";
 
-// function App() {
-//   const navigate = useNavigate();
-
-//   return (
-//     <Router>
-//       <div>
-//         <Routes>
-//           <Route
-//             path="/form"
-//             element={
-//               auth.firstLoginTimestamp ? <Form /> : navigate("/economist")
-//             }
-//           />
-//           <Route path="/economist" element={<EconomistLandingPage />} />
-//           <Route path="/gatekeeper" element={<GatekeeperLandingPage />} />
-//           <Route path="/authentication" element={<AuthenticationPage />} />
-//           <Route path="/*" element={<NotFoundPage />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
